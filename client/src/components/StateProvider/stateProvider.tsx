170b253b8fb3d3a4bd81c128836bffb8e35bbcaf
Mark Kraus <vindevccm@gmail.com>
2021-07-04 22:38:14 -0400

    trying to get types on context

--- client/src/components/StateProvider/stateProvider.tsx
+++ client/src/components/StateProvider/stateProvider.tsx
@@ -15,44 +15,45 @@ export const MyContext = createContext<{
 
 export const StateProvider = (props: any) => {
 
-    const [state, dispatch] = useReducer<any>(myReducer, defaultState);
+    const [state, dispatch] = useReducer(myReducer, defaultState);
 
-    const shuffleDeck: ()=> void = () => {
-        dispatch({ type: stateActions.SHUFFLE_DECK });
-    };
+    // const shuffleDeck: ()=> void = () => {
+    //     dispatch({ type: stateActions.SHUFFLE_DECK });
+    // };
 
-    const drawCard: ()=> void = () => {
-        dispatch({ type: stateActions.DRAW_CARD });
-    };
+    // const drawCard: ()=> void = () => {
+    //     dispatch({ type: stateActions.DRAW_CARD });
+    // };
 
-    const enterGame: ()=> void = () => {
-        dispatch({ type: stateActions.ENTER_GAME });
-    };
+    // const enterGame: ()=> void = () => {
+    //     dispatch({ type: stateActions.ENTER_GAME });
+    // };
 
-    const exitGame: ()=> void = () => {
-        dispatch({ type: stateActions.EXIT_GAME });
-    };
+    // const exitGame: ()=> void = () => {
+    //     dispatch({ type: stateActions.EXIT_GAME });
+    // };
 
-    const updateChatInput: ()=> void = (newChatText: string) => {
-        dispatch({ type: stateActions.UPDATE_CHAT_INPUT, payload: newChatText });
-    };
+    // const updateChatInput: ()=> void = (newChatText: string) => {
+    //     dispatch({ type: stateActions.UPDATE_CHAT_INPUT, payload: newChatText });
+    // };
 
-    const updateChatHistory: ()=> void = (newChatHistory: string[]) => {
-        dispatch({ type: stateActions.UPDATE_CHAT_HISTORY, payload: newChatHistory});
-    };
+    // const updateChatHistory: ()=> void = (newChatHistory: string[]) => {
+    //     dispatch({ type: stateActions.UPDATE_CHAT_HISTORY, payload: newChatHistory});
+    // };
 
-    const providerValue = {
-        ...state,
-        shuffleDeck,
-        drawCard,
-        enterGame,
-        exitGame,
-        updateChatInput,
-        updateChatHistory
-    };
+    // const providerValue = {
+    //     ...state,
+    //     shuffleDeck,
+    //     drawCard,
+    //     enterGame,
+    //     exitGame,
+    //     updateChatInput,
+    //     updateChatHistory
+    // };
 
     return (
-        <MyContext.Provider value={providerValue}>
+        <MyContext.Provider value={{state, dispatch}}>
+        {/* <MyContext.Provider value={providerValue}> */}
             {props.children}
         </MyContext.Provider>
     );
